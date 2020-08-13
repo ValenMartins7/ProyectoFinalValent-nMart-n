b51da0fe6237ce842a601040043dc83bfbbbfe4a
ValenMartins <valenmartin08@gmail.com>
2020-08-13 10:48:06 -0300

    agregando funcionalidad a los selectores de series

--- SelectorSeries.js
+++ SelectorSeries.js
@@ -4,5 +4,51 @@ class TabifitSelectorSeries extends HTMLElement {
         cargarTemplate("#tabifit-selectorseries", this);
     }
 }
-
 customElements.define("tabifit-selectorseries", TabifitSelectorSeries);
+
+
+
+function totalseries() {
+    const numeroseries = document.querySelector("#camposeries");
+    numeroseries.value = cantidadseries;
+}
+
+function sumarseries() {
+    cantidadseries += 1;
+    totalseries();
+}
+
+function restarseries() {
+     
+    cantidadseries -= 1;
+    totalseries();
+    
+}
+
+
+
+
+let cantidadseries = 1;
+
+totalseries();
+
+const aumentarserie = document.querySelector("#BotonSumaSeries");
+const reduceserie= document.querySelector("#BotonRestaSeries");
+
+aumentarserie.addEventListener("click", sumarseries);
+reduceserie.addEventListener("click", restarseries);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
